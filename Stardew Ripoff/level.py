f3ee9392d58fb0784464446494620c54707163e1
Brandon <91694942+CPScout22@users.noreply.github.com>
2022-12-02 14:00:46 -0700

    Update level.py

--- Stardew Ripoff/level.py
+++ Stardew Ripoff/level.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
-from player import Player
+from player import Player 
+from overlay import Overlay
 
 class Level:
     def __init__(self):
@@ -10,14 +11,19 @@ class Level:
         
         
         #class "group" is a part of pygame's sprite support. it is a class that manages a *list* of spries no cap.
+        
         self.all_sprites = pygame.sprite.Group()
+        
+        self.setup()
+        self.overlay = Overlay(self.player)
         
     def setup(self):
         self.player = Player((640, 360), self.all_sprites)
     
     
     def run(self, dt):
-        #print("Ran GAM")
+        
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface) # a draw function from the grp class
-        self.all_sprites.update() # another function from the grp class
+        self.all_sprites.draw(self.display_surface) # a draw function from the group class
+        self.all_sprites.update(dt) # another function from the group class
+        #print("RAN GAM")
