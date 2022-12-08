f7d0f9e6d0927c34be9a3512ac8e2b6dbffedd17
Brandon <91694942+CPScout22@users.noreply.github.com>
2022-12-08 14:48:39 -0700

    Update settings.py

--- Stardew Ripoff/settings.py
+++ Stardew Ripoff/settings.py
@@ -1,6 +1,20 @@
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 OVERLAY_POSITIONS = {
-    "tool" : (50, SCREEN_HEIGHT-15),
-    "seed" : (70, SCREEN_HEIGHT - 5)}
+    "tool" : (SCREEN_WIDTH, SCREEN_HEIGHT-15),
+    "seed" : (20, SCREEN_HEIGHT - 5)}
+
+LAYERS = {
+    "water": 0,
+    "ground": 1,
+    "soil": 2,
+    "soil water": 3,
+    "rain floor": 4,
+    "house bottom": 5,
+    "ground plant": 6,
+    "main": 7,
+    "house top": 8,
+    "fruit": 9,
+    "rain drops": 10
+    }
